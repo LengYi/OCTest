c8cffeaa7cd3d92944b523b1fd8ca96d2f611d38
戴亮金 <707689817@qq.com>
2017-11-09 18:32:26 +0800

    first commit

---
+++ OCTest/OCTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCTest
+//
+//  Created by ice on 2017/11/9.
+//  Copyright © 2017年 ice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCTest/OCTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCTest
+//
+//  Created by ice on 2017/11/9.
+//  Copyright © 2017年 ice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
